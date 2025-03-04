e21841e1d6706d54d1573c59f356c1b9b3305fbc
SquaredSypher <thisismyschoolemailman1@gmail.com>
2025-03-04 12:01:47 -0500

    adding some stuff and figuring out what the heck app.css is used for

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ function App() {
 
   return (
     <>
-      <div className='navbar'>
-        <button>Menu?</button>
-        <div className='Cash Reserve'>
-              1/1000
+      <div className='Navbar'>
+        <button className='MainMenuButton'>
+          <div className='MenuLine1'></div>
+          <div className='MenuLine2'></div>
+          <div className='MenuLine3'></div>
+        </button>
+        <div className='AllCashStats'>
+          <div className='CashLabel'>$500</div>
+          <div className='CashReserve'>
+            <div className='FillPercentage'></div>
+          </div>
         </div>
       </div>
     </>
